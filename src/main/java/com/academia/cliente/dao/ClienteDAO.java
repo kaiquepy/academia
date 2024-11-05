5a857a158a76858660a68b063e3eded40a1c23d2
Kaique Vieira <kvieiramiranda@gmail.com>
2024-11-05 09:09:57 -0300

    Adicione documentação aos métodos de ClienteDAO
    Inclui comentários de documentação JavaDoc para os métodos da interface ClienteDAO. Os comentários explicam a função de cada método e seus parâmetros, melhorando a compreensão do código e a manutenção futura.
    
    Signed-off-by: Kaique Vieira <kvieiramiranda@gmail.com>

--- src/main/java/com/academia/cliente/dao/ClienteDAO.java
+++ src/main/java/com/academia/cliente/dao/ClienteDAO.java
@@ -11,9 +11,39 @@ import java.util.List;
  * busca e listagem de clientes em um sistema de persistência de dados.
  */
 public interface ClienteDAO {
+    /**
+     * Adiciona um novo cliente.
+     *
+     * @param cliente Cliente a ser adicionado.
+     */
     void adicionarCliente(Cliente cliente);
+
+    /**
+     * Remove um cliente pelo CPF.
+     *
+     * @param cpf Cadastro de pessoa física do cliente.
+     */
     void removerCliente(String cpf);
+
+    /**
+     * Atualiza os dados de um cliente.
+     *
+     * @param cliente Cliente a ser atualizado.
+     */
     void atualizarCliente(Cliente cliente);
+
+    /**
+     * Busca um cliente pelo CPF.
+     *
+     * @param cpf Cadastro de pessoa física do cliente.
+     * @return Cliente
+     */
     Cliente buscarClientePorCpf(String cpf);
+
+    /**
+     * Lista todos os clientes cadastrados.
+     *
+     * @return List<Cliente>
+     */
     List<Cliente> listarClientes();
 }
