c31ecb70eed5d68b30a1ef61e411739875df7808
Kaique Vieira <kvieiramiranda@gmail.com>
2024-11-05 09:08:13 -0300

    Documenta métodos no ClienteController
    Adiciona comentários detalhados aos métodos da classe ClienteController para melhorar a compreensão do código. Esses comentários incluem descrições de propósito e detalhes sobre os parâmetros e retornos dos métodos.
    
    Signed-off-by: Kaique Vieira <kvieiramiranda@gmail.com>

--- src/main/java/com/academia/cliente/controller/ClienteController.java
+++ src/main/java/com/academia/cliente/controller/ClienteController.java
@@ -3,30 +3,64 @@ package com.academia.cliente.controller;
 import com.academia.cliente.dao.ClienteDAO;
 import com.academia.cliente.model.Cliente;
 
-
+/**
+ * Classe ClienteController representa a camada de controle de clientes.
+ *
+ * Esta classe é responsável por intermediar a comunicação entre a camada de
+ * visão e a camada de persistência de dados
+ */
 public class ClienteController {
     private final ClienteDAO clienteDAO;
 
+    /**
+     * Construtor da classe ClienteController.
+     *
+     * @param clientDAO DAO de cliente.
+     */
     public ClienteController(ClienteDAO clientDAO) {
         this.clienteDAO = clientDAO;
     }
 
+    /**
+     * Cadastra um novo cliente.
+     *
+     * @param cliente Cliente a ser cadastrado.
+     */
     public void cadastrarCliente(Cliente cliente) {
         clienteDAO.adicionarCliente(cliente);
     }
 
+    /**
+     * Atualiza os dados de um cliente.
+     *
+     * @param cliente Cliente a ser atualizado.
+     */
     public void atualizarCliente(Cliente cliente) {
         clienteDAO.atualizarCliente(cliente);
     }
 
+    /**
+     * Remove um cliente pelo CPF.
+     *
+     * @param cpf Cadastro de pessoa física do cliente.
+     */
     public void removerCliente(String cpf) {
         clienteDAO.removerCliente(cpf);
     }
 
+    /**
+     * Busca um cliente pelo CPF.
+     *
+     * @param cpf Cadastro de pessoa física do cliente.
+     * @return Cliente
+     */
     public Cliente buscarCliente(String cpf) {
         return clienteDAO.buscarClientePorCpf(cpf);
     }
 
+    /**
+     * Lista todos os clientes cadastrados.
+     */
     public void listarClientes() {
         clienteDAO.listarClientes().forEach(c -> System.out.println("Cliente: " + c.getNome()));
     }
