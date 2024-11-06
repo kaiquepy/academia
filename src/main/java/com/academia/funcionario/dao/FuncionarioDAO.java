aa22c05291b9d902bab4d23114fa319afce70754
Kaique Vieira <kvieiramiranda@gmail.com>
2024-11-06 12:28:34 -0300

    Adicionando interface FuncionarioDAO
    Criei a interface FuncionarioDAO para definir operações de acesso a dados relacionadas à entidade Funcionario. Essa interface inclui métodos para adição, remoção, atualização, busca e listagem de funcionários.
    
    Signed-off-by: Kaique Vieira <kvieiramiranda@gmail.com>

---
+++ src/main/java/com/academia/funcionario/dao/FuncionarioDAO.java
@@ -0,0 +1,49 @@
+package com.academia.funcionario.dao;
+
+import com.academia.funcionario.model.Funcionario;
+
+import java.util.ArrayList;
+
+/**
+ * Interface FuncionarioDAO representa as operações de acesso a dados para a entidade Funcionario.
+ *
+ * Métodos desta interface permitem a adição, remoção, atualização,
+ * busca e listagem de funcionários em um sistema de persistência de dados.
+ */
+public interface FuncionarioDAO {
+    /**
+     * Adiciona um novo funcionário.
+     *
+     * @param funcionario Funcionário a ser adicionado.
+     */
+    void adicionarFuncionario(Funcionario funcionario);
+
+    /**
+     * Remove um funcionário pelo ID.
+     *
+     * @param id Identificador do funcionário.
+     */
+    void removerFuncionario(int id);
+
+    /**
+     * Atualiza os dados de um funcionário.
+     *
+     * @param funcionario Funcionário a ser atualizado.
+     */
+    void atualizarFuncionario(Funcionario funcionario);
+
+    /**
+     * Busca um funcionário pelo ID.
+     *
+     * @param id Identificador do funcionário.
+     * @return Funcionario
+     */
+    Funcionario buscarFuncionarioPorID(int id);
+
+    /**
+     * Lista todos os funcionários cadastrados.
+     *
+     * @return List<Funcionario>
+     */
+    ArrayList<Funcionario> listarFuncionarios();
+}
