ca5eff12006076534ec7baed25506e13b643f3ac
Kaique Vieira <kvieiramiranda@gmail.com>
2024-11-06 12:28:05 -0300

    Adicionando classe Funcionario ao projeto
    Inclui a classe Funcionario com atributos básicos como id, nome, cargo e salário. Esta classe também possui métodos getters e setters, além de um método toString para exibir suas informações.
    
    Signed-off-by: Kaique Vieira <kvieiramiranda@gmail.com>

---
+++ src/main/java/com/academia/funcionario/model/Funcionario.java
@@ -0,0 +1,72 @@
+package com.academia.funcionario.model;
+
+
+/**
+ * A classe Funcionário representa uma entidade com informações básicas de um funcionário.
+ * Ela possui atributos como nome, cargo e salário.
+ */
+public class Funcionario {
+    private int id;
+    private String nome;
+    private String cargo;
+    private double salario;
+
+    /**
+     * Construtor da classe Funcionário.
+     *
+     * @param id      Identificador do funcionário.
+     * @param nome    Nome do funcionário.
+     * @param cargo   Cargo do funcionário.
+     * @param salario Salário do funcionário.
+     */
+    public Funcionario(int id, String nome, String cargo, double salario) {
+        this.id = id;
+        this.nome = nome;
+        this.cargo = cargo;
+        this.salario = salario;
+    }
+
+    // region Getters and Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+    // endregion
+
+    @Override
+    public String toString() {
+        return "Funcionario{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", cargo='" + cargo + '\'' +
+                ", salario=" + salario +
+                '}';
+    }
+}
