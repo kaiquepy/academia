05dbae69faae94d886c9014ceccbd6c237882e46
Kaique Vieira <kvieiramiranda@gmail.com>
2024-11-09 12:16:30 -0300

    Adicionando dependência Gson e configurando a abertura de novos pacotes
    Inclua a biblioteca com.google.gson no módulo e abra os pacotes com.academia.funcionario.model e com.academia.cliente.model para Gson. Isso permitirá a serialização e desserialização de objetos desses pacotes usando Gson.
    
    Signed-off-by: Kaique Vieira <kvieiramiranda@gmail.com>

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,8 +2,11 @@ module com.academia {
     requires javafx.controls;
     requires javafx.fxml;
     requires org.xerial.sqlitejdbc;
+    requires com.google.gson;
 
 
     opens com.academia to javafx.fxml;
+    opens com.academia.funcionario.model to com.google.gson;
+    opens com.academia.cliente.model to com.google.gson;
     exports com.academia;
 }
