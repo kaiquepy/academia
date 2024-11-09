c65196a1deadb6cc6cf104c66c438d767b2ec1ab
Kaique Vieira <kvieiramiranda@gmail.com>
2024-11-09 16:52:35 -0300

    Adicionando interface SalaDAO para operações CRUD de salas
    Implementada a interface SalaDAO com métodos para adicionar, remover, listar, buscar por ID e atualizar registros de salas. Esta adição é fundamental para a abstração e padronização das operações de acesso aos dados das salas no sistema.
    
    Signed-off-by: Kaique Vieira <kvieiramiranda@gmail.com>

---
+++ src/main/java/com/academia/sala/dao/SalaDAO.java
@@ -0,0 +1,48 @@
+package com.academia.sala.dao;
+
+import com.academia.sala.model.Sala;
+
+import java.util.List;
+
+/**
+ * Interface que define os métodos que devem ser implementados por uma classe que realiza operações de CRUD em uma sala.
+ */
+public interface SalaDAO {
+    /**
+     * Adiciona uma nova sala.
+     *
+     * @param sala Sala a ser adicionada.
+     */
+    void adicionarSala(Sala sala);
+
+    /**
+     * Remove uma sala pelo ID.
+     *
+     * @param id Identificador da sala.
+     * @return boolean
+     */
+    boolean removerSala(int id);
+
+    /**
+     * Lista todas as salas cadastradas.
+     *
+     * @return List<Sala>
+     */
+    List<Sala> listarSalas();
+
+    /**
+     * Busca uma sala pelo ID.
+     *
+     * @param id Identificador da sala.
+     * @return Sala
+     */
+    Sala buscarSalaPorId(int id);
+
+    /**
+     * Atualiza os dados de uma sala.
+     *
+     * @param sala Sala a ser atualizada.
+     * @return boolean
+     */
+    boolean atualizarSala(Sala sala);
+}
