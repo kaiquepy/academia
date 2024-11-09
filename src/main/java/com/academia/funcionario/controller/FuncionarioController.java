41a07de4e5d7aa97937381f090c62a493d98cb88
Kaique Vieira <kvieiramiranda@gmail.com>
2024-11-09 15:33:28 -0300

    Configurando acesso ao DAO com base no tipo de armazenamento
    Adiciona lógica para selecionar entre FuncionarioDAOJSON e FuncionarioDAOMySQL com base na configuração de armazenamento. Trata possíveis exceções ao carregar o arquivo de configuração, garantindo uma inicialização segura.
    
    Signed-off-by: Kaique Vieira <kvieiramiranda@gmail.com>

--- src/main/java/com/academia/funcionario/controller/FuncionarioController.java
+++ src/main/java/com/academia/funcionario/controller/FuncionarioController.java
@@ -1,15 +1,30 @@
 package com.academia.funcionario.controller;
 
 import com.academia.funcionario.dao.FuncionarioDAO;
+import com.academia.funcionario.dao.FuncionarioDAOJSON;
+import com.academia.funcionario.dao.FuncionarioDAOMySQL;
 import com.academia.funcionario.model.Funcionario;
+import com.academia.utils.Config;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Properties;
 
 public class FuncionarioController {
     private final FuncionarioDAO funcionarioDAO;
 
-    public FuncionarioController(FuncionarioDAO funcionarioDAO) {
-        this.funcionarioDAO = funcionarioDAO;
+    public FuncionarioController() {
+        try {
+            Properties config = Config.getProperties();
+
+            if (config.getProperty("storage.type").equals("json")) {
+                this.funcionarioDAO = new FuncionarioDAOJSON();
+            } else {
+                this.funcionarioDAO = new FuncionarioDAOMySQL();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Erro ao carregar arquivo de configuração.");
+        }
     }
 
     public void cadastrarFuncionario(Funcionario funcionario) {
