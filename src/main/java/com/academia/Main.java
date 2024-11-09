1a332d8ee4b57608c993b569eccf9732fbd2d855
Kaique Vieira <kvieiramiranda@gmail.com>
2024-11-09 15:35:06 -0300

    Atualizando main para chamar o dashboad
    Substitui a exibição de menu de ClienteConsoleView para DashboardConsoleView, com verificação do tipo de visualização via configurações. Também adiciona tratamento de exceções para melhorar a robustez do código.
    
    Signed-off-by: Kaique Vieira <kvieiramiranda@gmail.com>

--- src/main/java/com/academia/Main.java
+++ src/main/java/com/academia/Main.java
@@ -1,16 +1,22 @@
 package com.academia;
 
-import com.academia.cliente.controller.ClienteController;
-import com.academia.cliente.dao.ClienteDAO;
-import com.academia.cliente.dao.ClienteDAOMySQL;
-import com.academia.cliente.view.console.ClienteConsoleView;
+import com.academia.dashboard.view.console.DashboardConsoleView;
+import com.academia.utils.Config;
+
+import java.util.Properties;
 
 
 public class Main {
     public static void main(String[] args) {
-        ClienteDAO clienteDAO = new ClienteDAOMySQL();
-        ClienteController clienteController = new ClienteController(clienteDAO);
-
-        ClienteConsoleView.exibirMenu(clienteController);
+        try {
+            Properties properties = Config.getProperties();
+            if (properties.getProperty("view.type").equals("console")) {
+                DashboardConsoleView.exibirMenu();
+            } else {
+                System.out.println("Tipo de visualização não suportado.");
+            }
+        } catch (Exception e) {
+            System.out.println("Erro ao executar o sistema: " + e.getMessage());
+        }
     }
 }
