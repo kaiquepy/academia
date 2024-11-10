6d766150921c3e7c36b68ff5714e00a8e13477cc
Kaique Vieira <kvieiramiranda@gmail.com>
2024-11-10 17:40:09 -0300

    Adicionando JavaDocs e método toString na classe Config
    Adicionei comentários JavaDocs para a documentação da classe Config e de seus métodos. Além disso, sobrescrevi o método toString para fornecer uma descrição da classe.
    
    Signed-off-by: Kaique Vieira <kvieiramiranda@gmail.com>

--- src/main/java/com/academia/utils/Config.java
+++ src/main/java/com/academia/utils/Config.java
@@ -4,11 +4,30 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
 
+/**
+ * Classe para carregar as configurações do sistema.
+ */
 public class Config {
+    /**
+     * Método para obter as propriedades do arquivo de configuração.
+     *
+     * @return Propriedades do arquivo de configuração.
+     * @throws IOException Caso ocorra um erro ao carregar o arquivo de configuração.
+     */
     public static Properties getProperties() throws IOException {
         Properties properties = new Properties();
         FileInputStream fileInputStream = new FileInputStream("src/main/resources/application.properties");
         properties.load(fileInputStream);
         return properties;
+    }
+
+    /**
+     * Sobrescreve o método toString para retornar uma descrição da classe.
+     *
+     * @return Descrição da classe.
+     */
+    @Override
+    public String toString() {
+        return "Classe para carregar as configurações do sistema.";
     }
 }
