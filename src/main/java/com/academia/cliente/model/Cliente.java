014f0c795b1002b0fd11cbedfcd735d7656f0ce2
Kaique Vieira <kvieiramiranda@gmail.com>
2024-11-10 21:31:13 -0300

    Adicionando contadores de instância à classe Cliente
    Implementa dois contadores de instâncias, um protegido e um privado, na classe Cliente. Adiciona métodos para acessar e incrementar esses contadores, além de comentários de documentação para diversos métodos.
    
    Signed-off-by: Kaique Vieira <kvieiramiranda@gmail.com>

--- src/main/java/com/academia/cliente/model/Cliente.java
+++ src/main/java/com/academia/cliente/model/Cliente.java
@@ -6,6 +6,9 @@ package com.academia.cliente.model;
  * Ela possui atributos como nome, CPF, endereço, telefone e e-mail.
  */
 public class Cliente {
+    protected static int contadorInstanciasProtected = 0;
+    private static int contadorInstanciasPrivate = 0;
+
     private int id;
     private String nome;
     private String cpf;
@@ -30,58 +33,137 @@ public class Cliente {
         this.endereco = endereco;
         this.telefone = telefone;
         this.email = email;
+        contadorInstanciasProtected++;
+        incrementarContadorInstanciasPrivate();
+    }
+
+    public static int getContadorInstanciasProtected() {
+        return contadorInstanciasProtected;
+    }
+
+    private static void incrementarContadorInstanciasPrivate() {
+        contadorInstanciasPrivate++;
+    }
+
+    public static int getContadorInstanciasPrivate() {
+        return contadorInstanciasPrivate;
     }
 
     // region Getters and Setters
+    /**
+     * Retorna o identificador do cliente.
+     *
+     * @return int Id do cliente.
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     * Define o identificador do cliente.
+     *
+     * @param id Identificador do cliente.
+     */
     public void setId(int id) {
         this.id = id;
     }
 
+    /**
+     * Retorna o nome do cliente.
+     *
+     * @return String Nome do cliente.
+     */
     public String getNome() {
         return nome;
     }
 
+    /**
+     * Define o nome do cliente.
+     *
+     * @param nome Nome do cliente.
+     */
     public void setNome(String nome) {
         this.nome = nome;
     }
 
+    /**
+     * Retorna o CPF do cliente.
+     *
+     * @return String CPF do cliente.
+     */
     public String getCpf() {
         return cpf;
     }
 
+    /**
+     * Define o CPF do cliente.
+     *
+     * @param cpf CPF do cliente.
+     */
     public void setCpf(String cpf) {
         this.cpf = cpf;
     }
 
+    /**
+     * Retorna o endereço do cliente.
+     *
+     * @return String Endereço do cliente.
+     */
     public String getEndereco() {
         return endereco;
     }
 
+    /**
+     * Define o endereço do cliente.
+     *
+     * @param endereco Endereço do cliente.
+     */
     public void setEndereco(String endereco) {
         this.endereco = endereco;
     }
 
+    /**
+     * Retorna o telefone do cliente.
+     *
+     * @return String Telefone do cliente.
+     */
     public String getTelefone() {
         return telefone;
     }
 
+    /**
+     * Define o telefone do cliente.
+     *
+     * @param telefone Telefone do cliente.
+     */
     public void setTelefone(String telefone) {
         this.telefone = telefone;
     }
 
+    /**
+     * Retorna o e-mail do cliente.
+     *
+     * @return String E-mail do cliente.
+     */
     public String getEmail() {
         return email;
     }
 
+    /**
+     * Define o e-mail do cliente.
+     *
+     * @param email E-mail do cliente.
+     */
     public void setEmail(String email) {
         this.email = email;
     }
     // endregion
 
+    /**
+     * Retorna a representação em String do objeto Cliente.
+     *
+     * @return String representando o objeto.
+     */
     @Override
     public String toString() {
         return "Cliente{" +
