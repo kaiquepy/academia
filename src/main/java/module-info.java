286412931fd7fb69af0468dda70d28b7fad55933
Kaique Vieira <kvieiramiranda@gmail.com>
2024-11-10 21:36:24 -0300

    Adicionando novos pacotes ao módulo de abertura
    Inclua os pacotes com.academia.loja.model, com.academia.produto.model, com.academia.estoque.model, e com.academia.usuario.model para abertura com Google Gson. Isso facilita a serialização e desserialização dos modelos nessas novas áreas.
    
    Signed-off-by: Kaique Vieira <kvieiramiranda@gmail.com>

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -9,5 +9,9 @@ module com.academia {
     opens com.academia.funcionario.model to com.google.gson;
     opens com.academia.cliente.model to com.google.gson;
     opens com.academia.sala.model to com.google.gson;
+    opens com.academia.loja.model to com.google.gson;
+    opens com.academia.produto.model to com.google.gson;
+    opens com.academia.estoque.model to com.google.gson;
+    opens com.academia.usuario.model to com.google.gson;
     exports com.academia;
 }
